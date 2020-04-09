a66c0e36b00f1958c7ac5b6fe2be6f646b22c844
devanshu <devanshu.vguj@gmail.com>
2020-04-09 20:30:29 +0530

    Demo Only

---
+++ mappings.js
@@ -0,0 +1,61 @@
+var client = require('./connection.js');
+
+client.indices.putMapping({  
+  index: 'gov',
+  type: 'constituencies',
+  body: {
+    properties: {
+      'constituencyname': {
+        'type': 'string', // type is a required attribute if index is specified
+        'index': false
+      },
+      'electorate': {  
+        'type': 'integer'
+      },
+      'validvotes': {  
+        'type': 'integer'
+      }
+    }
+  }
+},function(err,resp,status){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(resp);
+    }
+});
+
+client.indices.putMapping({  
+  index: 'gov',
+  type: 'petitions',
+  body: {
+    properties: {
+      'signatures_by_constituency': {
+        'type': 'nested',
+        properties: {
+          'name': {
+            'type': 'string',
+            'index': 'not_analyzed'
+          }
+        }
+      },
+      'signatures_by_country': {
+        'type': 'nested',
+        properties: {
+          'name': {
+            'type': 'string',
+            'index': 'not_analyzed'
+          }
+        }
+      }
+    }
+  }
+},function(err,resp){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(resp);
+    }
+});
